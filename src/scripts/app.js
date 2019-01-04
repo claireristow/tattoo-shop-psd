ef5989fba9f495c2ef3f6dc0c8307c563a1cb801
Claire <claire.ristow@gmail.com>
2019-01-04 15:32:55 -0500

    about and faq page added

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,33 @@
-const hello = 'World';
-console.log(hello);
+
+const app = {};
+
+app.events = function() {
+    
+    // when a hamburger nav is clicked
+    document.querySelector('.menuCircle').addEventListener('click', function(e) {
+        const menu = document.querySelector('.menu');
+        if (menu.classList.contains('showFlex')) {
+            menu.classList.remove('showFlex');
+        } else {
+            menu.classList.add('showFlex');
+        }
+    })
+
+    // when a faq is clicked
+    if (document.getElementById('questionItem')) {
+        document.getElementById('questionItem').addEventListener('click', function(e) {
+            const clickedItem = this;
+            console.log(clickedItem);
+        })
+    }
+
+
+}
+
+app.init = function() {
+    app.events();
+}; 
+
+(function() {
+    app.init();
+})();
